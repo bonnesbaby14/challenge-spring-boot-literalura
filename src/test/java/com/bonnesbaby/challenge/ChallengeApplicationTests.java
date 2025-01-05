cc4a83f6932f02bfa9e7307f0cd863283675008e
bonnesbaby14 <bonnesbaby14@gmail.com>
2025-01-05 14:04:08 -0600

    first commit

---
+++ src/test/java/com/bonnesbaby/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bonnesbaby.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
